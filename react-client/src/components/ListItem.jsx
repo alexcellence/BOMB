8b7276c888d04e08907b67525640f6c8c99bf5de
Alex Wu <alexcellence@gmail.com>
2020-09-24 01:40:11 -0700

    Change CSS and font of list items

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const ListElement = styled.li`
   font-size: 20px;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
 `
 
 const ListItem = (props) => (
